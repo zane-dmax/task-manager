717a3a2bc5fa106fb6527c85ab42771f499195d8
ZaneH <zane@dentimax.com>
2019-12-23 14:43:34 -0700

    12.23.19_w1

--- src/index.js
+++ src/index.js
@@ -11,8 +11,27 @@ app.post('/users', (req, res) => {
 
     user.save().then(() => {
         res.status(201).send(user)
-    }).catch((error) => {
-        res.status(400).send(error)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
+app.get('/users', (req, res) => {
+    User.findAll().then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+    
+    User.findByPk(_id).then((user) => {
+        if (!user) { return res.status(404).send() }
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send()
     })
 })
 
@@ -21,8 +40,27 @@ app.post('/tasks', (req, res) => {
 
     task.save().then(() => {
         res.status(201).send(task)
-    }).catch((error) => {
-        res.status(400).send(error)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
+app.get('/tasks', (req, res) => {
+    Task.findAll().then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+    
+    Task.findByPk(_id).then((task) => {
+        if (!task) { return res.status(404).send() }
+        res.send(task)
+    }).catch((e) => {
+        res.status(500).send()
     })
 })
 
