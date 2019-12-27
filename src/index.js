a34c089c8de5a665a733400d49f7fd17a8830795
ZaneH <zane@dentimax.com>
2019-12-27 16:02:55 -0700

    12.27.19w

--- src/index.js
+++ src/index.js
@@ -6,35 +6,69 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', (req, res) => {
+////////////////////////////////////////////////////////////////////////////////////////////////////
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
 
-    user.save().then(() => {
+    try {
+        await user.save()
         res.status(201).send(user)
-    }).catch((e) => {
+    } catch (e) {
         res.status(400).send(e)
-    })
+    }
 })
 
-app.get('/users', (req, res) => {
-    User.findAll().then((users) => {
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.findAll({})
         res.send(users)
-    }).catch((e) => {
+    } catch (e) {
         res.status(500).send()
-    })
+    }
 })
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id
     
-    User.findByPk(_id).then((user) => {
+    try {
+        const user = await User.findByPk(_id)
         if (!user) { return res.status(404).send() }
         res.send(user)
-    }).catch((e) => {
+    } catch (e) {
         res.status(500).send()
-    })
+    }
 })
 
+app.patch('/users/:id', async (req, res) => {
+    const pkeys = Object.keys(req.body)
+    const vkeys = Object.keys(User.rawAttributes)
+    const isValid = pkeys.every((key) => vkeys.includes(key))
+
+    if (!isValid) {
+        return res.status(400).send({ error: "One or more of the provided properties does not belong to User" })
+    }
+
+    try {
+        const changed = await User.update(req.body,{ where: {id: req.params.id} })
+        if (changed < 1) { return res.status(404).send() }
+        res.send(changed)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByPk(req.params.id)
+        if (!user) { return res.status(404).send() }
+        await user.destroy()
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
@@ -64,8 +98,36 @@ app.get('/tasks/:id', (req, res) => {
     })
 })
 
+app.patch('/tasks/:id', async (req, res) => {
+    const pkeys = Object.keys(req.body)
+    const vkeys = Object.keys(Task.rawAttributes)
+    const isValid = pkeys.every((key) => vkeys.includes(key))
+
+    if (!isValid) {
+        return res.status(400).send({ error: "One or more of the provided properties does not belong to Task" })
+    }
+
+    try {
+        const changed = await Task.update(req.body,{ where: {id: req.params.id} })
+        if (changed < 1) { return res.status(404).send() }
+        res.send(changed)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByPk(req.params.id)
+        if (!task) { return res.status(404).send() }
+        await task.destroy()
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 app.listen(port, () => {
     console.log('Server up on port ' + port)
 })
-
-//test
